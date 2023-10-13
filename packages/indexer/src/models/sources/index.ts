37dc93bf522d330646ded35601feb8c9df8ff518
forward <qq1506430045@163.com>
2023-10-13 17:26:52 +0800

    manta-testnet

--- packages/indexer/src/models/sources/index.ts
+++ packages/indexer/src/models/sources/index.ts
@@ -539,6 +539,16 @@ export class Sources {
 
         return _.replace(sourceEntity.metadata.url, "${tokenId}", tokenId);
       }
+    } else if (config.chainId == 3441005) {
+      if (sourceEntity.metadata.tokenUrlMantaTestnet && contract && tokenId) {
+        sourceEntity.metadata.url = _.replace(
+            sourceEntity.metadata.tokenUrlMantaTestnet,
+            "${contract}",
+            contract
+        );
+
+        return _.replace(sourceEntity.metadata.url, "${tokenId}", tokenId);
+      }
     } else {
       if (sourceEntity.metadata.tokenUrlGoerli && contract && tokenId) {
         sourceEntity.metadata.url = _.replace(
