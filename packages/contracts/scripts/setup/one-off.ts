404d299ed7f3174b77b697d36fecfb984638616a
forward <qq1506430045@163.com>
2023-10-14 16:48:13 +0800

    manta-mainnet

--- packages/contracts/scripts/setup/one-off.ts
+++ packages/contracts/scripts/setup/one-off.ts
@@ -2,19 +2,23 @@
 
 import { ethers } from "hardhat";
 
-import { DEPLOYER, trigger } from "./trigger";
+import { DEPLOYER, readDeployment, trigger } from "./trigger";
 
 const main = async () => {
   const chainId = await ethers.provider.getNetwork().then((n) => n.chainId);
 
-  // Make sure the current signer is the canonical deployer
-  const [deployer] = await ethers.getSigners();
-  if (deployer.address.toLowerCase() !== DEPLOYER.toLowerCase()) {
-    throw new Error("Wrong deployer");
-  }
+  // // Make sure the current signer is the canonical deployer
+  // const [deployer] = await ethers.getSigners();
+  // if (deployer.address.toLowerCase() !== DEPLOYER.toLowerCase()) {
+  //   throw new Error("Wrong deployer");
+  // }
+  //
+  // chainId;
+  // trigger;
 
-  chainId;
-  trigger;
+    // 部署 router
+    const address = await trigger.Modules.AlienswapModule(chainId);
+    console.log("xxxxx", address)
 };
 
 main()
